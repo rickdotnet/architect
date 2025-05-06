cf54ccd8fe264836d90b7fd622dd335d92085755
Rick <rick@rickdot.net>
2025-05-06 14:22:40 -0500

    initial port from poc

---
+++ src/Cli/AppConfig.cs
@@ -0,0 +1,6 @@
+namespace Architect.Cli;
+
+public record AppConfig
+{
+    public string MyValue { get; init; } = "config value";
+}
---
+++ src/Cli/Program.cs
@@ -0,0 +1,23 @@
+﻿using Architect.Cli;
+using Architect.Cli.Commands;
+using Microsoft.Extensions.Hosting;
+using ConsoleAppFramework;
+using SimpleExec;
+
+// will launch into the CLI using various flags
+// there are a few options, looking at Terminal.Gui
+// first, but then also looking at the one from nuecc
+// and team.
+
+// only some of them will run the host, and at that,
+// we'll wrap it and make sure we're smart about how
+// hosts are created
+var app = ConsoleApp.Create();
+app.Add<Application>(); // base application
+app.Add<Install>("install"); // tools
+app.Run(args);
+
+
+
+// var host = Host.CreateApplicationBuilder(args).ConfigureHost();
+// host.Run();
